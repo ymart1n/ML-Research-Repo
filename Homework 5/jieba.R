0f280f53e5075f171fcdeff8241fae36641f4fc9
Zhaohang Yan <10969mart1n@gmail.com>
2020-11-28 12:13:14 +0800

    adding content of hw5

--- Homework 5/jieba.R
+++ Homework 5/jieba.R
@@ -3,6 +3,7 @@
 # install.packages("pacman")
 # install.packages("dplyr")
 # install.packages("stringi", configure.vars="ICUDT_DIR=./icudt61l")
+# install.packages("devtools")
 
 library(pacman)
 library(readr)
@@ -11,28 +12,30 @@ p_load(tidyverse,jiebaR)
 p_load(tidytext)
 library(jiebaR)
 
-test_string <- read_file("./Homework 5/test.txt")
-worker() -> wk
-segment(test_string, wk) -> mixseg
+test_string <- read_file("./Homework 5/testML.txt")
+# worker() -> wk
+# segment(test_string, wk) -> mixseg
 
 # mixseg
 
-tagger = worker("tag") #开启词性标注启发器
-vector_tag(mixseg, tagger) -> tag_result
+# tagger = worker("tag") #开启词性标注启发器
+# vector_tag(mixseg, tagger) -> tag_result
 
-tag_result
+# tag_result
 
-str(tag_result)
+# str(tag_result)
 
-tag_table <- enframe(tag_result)
-tag_table
+# tag_table <- enframe(tag_result)
+# tag_table
 
-keys = worker("keywords",topn = 10, idf = IDFPATH)
-keys <= test_string
+keys = worker("keywords",topn = 100, idf = IDFPATH)
+keywords <- keys <= test_string
+str(keywords)
 
+top_table <- data.frame(word=keywords, tf_idf=as.numeric(names(keywords)), row.names=NULL)
 
-p_load(wordcloud2)
-tag_table %>% 
-  top_n(10) %>% 
-  wordcloud2(size = 2, fontFamily = "微软雅黑",
-           color = "random-light", backgroundColor = "grey")
+library(devtools)
+devtools::install_github("lchiffon/wordcloud2")
+library(wordcloud2)
+
+wordcloud2(top_table, size = 2, fontFamily = "微软雅黑", color = "random-light", backgroundColor = "grey")
